38d3eba36251b7046bc836e69ecef5ab80c42822
lucavisinelli <luca.visinelli@gmail.com>
2021-03-20 18:51:40 +0100

    Update WhiskerPlot.py

--- code/WhiskerPlot.py
+++ code/WhiskerPlot.py
@@ -3,7 +3,6 @@ from collections import Counter
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
-from getdist import plots
 black = [0,0,0]
 red   = [1,0,0]
 blue  = [0,0,1]
